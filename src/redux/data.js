c2faed6dce6cdfc93664aa37c342dc3fbdd7f98c
Illia Ilin <sunrise.th13@gmail.com>
2021-02-06 13:49:35 +0200

    Memories project added to portfolio

--- src/redux/data.js
+++ src/redux/data.js
@@ -1,51 +1,57 @@
-import applabTitle from "../assets/projectImages/applabTitle.jpg";
-import applabJumbo from "../assets/projectImages/applabJumbo.jpg";
-import applabShowcaseImg1 from "../assets/projectImages/applabShowcaseImg1.jpg";
-import applabShowcaseImg2 from "../assets/projectImages/applabShowcaseImg2.jpg";
-import applabShowcaseImg3 from "../assets/projectImages/applabShowcaseImg3.jpg";
+import applabTitle from '../assets/projectImages/applabTitle.jpg';
+import applabJumbo from '../assets/projectImages/applabJumbo.jpg';
+import applabShowcaseImg1 from '../assets/projectImages/applabShowcaseImg1.jpg';
+import applabShowcaseImg2 from '../assets/projectImages/applabShowcaseImg2.jpg';
+import applabShowcaseImg3 from '../assets/projectImages/applabShowcaseImg3.jpg';
 
-import axitTitle from "../assets/projectImages/axitTitle.jpg";
-import axitJumbo from "../assets/projectImages/axitJumbo.jpg";
-import axitShowcaseImg1 from "../assets/projectImages/axitShowcaseImg1.jpg";
-import axitShowcaseImg2 from "../assets/projectImages/axitShowcaseImg2.jpg";
-import axitShowcaseImg3 from "../assets/projectImages/axitShowcaseImg3.jpg";
+import axitTitle from '../assets/projectImages/axitTitle.jpg';
+import axitJumbo from '../assets/projectImages/axitJumbo.jpg';
+import axitShowcaseImg1 from '../assets/projectImages/axitShowcaseImg1.jpg';
+import axitShowcaseImg2 from '../assets/projectImages/axitShowcaseImg2.jpg';
+import axitShowcaseImg3 from '../assets/projectImages/axitShowcaseImg3.jpg';
 
-import leospaTitle from "../assets/projectImages/leospaTitle.jpg";
-import leospaJumbo from "../assets/projectImages/leospaJumbo.jpg";
-import leospaShowcaseImg1 from "../assets/projectImages/leospaShowcaseImg1.jpg";
-import leospaShowcaseImg2 from "../assets/projectImages/leospaShowcaseImg2.jpg";
-import leospaShowcaseImg3 from "../assets/projectImages/leospaShowcaseImg3.jpg";
+import leospaTitle from '../assets/projectImages/leospaTitle.jpg';
+import leospaJumbo from '../assets/projectImages/leospaJumbo.jpg';
+import leospaShowcaseImg1 from '../assets/projectImages/leospaShowcaseImg1.jpg';
+import leospaShowcaseImg2 from '../assets/projectImages/leospaShowcaseImg2.jpg';
+import leospaShowcaseImg3 from '../assets/projectImages/leospaShowcaseImg3.jpg';
 
-import minimoTitle from "../assets/projectImages/minimoTitle.jpg";
-import minimoJumbo from "../assets/projectImages/minimoJumbo.jpg";
-import minimoShowcaseImg1 from "../assets/projectImages/minimoShowcaseImg1.jpg";
-import minimoShowcaseImg2 from "../assets/projectImages/minimoShowcaseImg2.jpg";
-import minimoShowcaseImg3 from "../assets/projectImages/minimoShowcaseImg3.jpg";
+import minimoTitle from '../assets/projectImages/minimoTitle.jpg';
+import minimoJumbo from '../assets/projectImages/minimoJumbo.jpg';
+import minimoShowcaseImg1 from '../assets/projectImages/minimoShowcaseImg1.jpg';
+import minimoShowcaseImg2 from '../assets/projectImages/minimoShowcaseImg2.jpg';
+import minimoShowcaseImg3 from '../assets/projectImages/minimoShowcaseImg3.jpg';
 
-import redditTitle from "../assets/projectImages/redditTitle.jpg";
-import redditJumbo from "../assets/projectImages/redditJumbo.jpg";
-import redditShowcaseImg1 from "../assets/projectImages/redditShowcaseImg1.jpg";
-import redditShowcaseImg2 from "../assets/projectImages/redditShowcaseImg2.png";
-import redditShowcaseImg3 from "../assets/projectImages/redditShowcaseImg3.jpg";
+import redditTitle from '../assets/projectImages/redditTitle.jpg';
+import redditJumbo from '../assets/projectImages/redditJumbo.jpg';
+import redditShowcaseImg1 from '../assets/projectImages/redditShowcaseImg1.jpg';
+import redditShowcaseImg2 from '../assets/projectImages/redditShowcaseImg2.png';
+import redditShowcaseImg3 from '../assets/projectImages/redditShowcaseImg3.jpg';
 
-import silonTitle from "../assets/projectImages/silonTitle.jpg";
-import silonJumbo from "../assets/projectImages/silonJumbo.jpg";
-import silonShowcaseImg1 from "../assets/projectImages/silonShowcaseImg1.jpg";
-import silonShowcaseImg2 from "../assets/projectImages/silonShowcaseImg2.jpg";
-import silonShowcaseImg3 from "../assets/projectImages/silonShowcaseImg3.jpg";
+import silonTitle from '../assets/projectImages/silonTitle.jpg';
+import silonJumbo from '../assets/projectImages/silonJumbo.jpg';
+import silonShowcaseImg1 from '../assets/projectImages/silonShowcaseImg1.jpg';
+import silonShowcaseImg2 from '../assets/projectImages/silonShowcaseImg2.jpg';
+import silonShowcaseImg3 from '../assets/projectImages/silonShowcaseImg3.jpg';
+
+import memoriesTitle from '../assets/projectImages/memoriesTitle.jpg';
+import memoriesJumbo from '../assets/projectImages/memoriesJumbo.jpg';
+import memoriesShowcaseImg1 from '../assets/projectImages/memoriesShowcaseImg1.jpg';
+import memoriesShowcaseImg2 from '../assets/projectImages/memoriesShowcaseImg2.jpg';
+import memoriesShowcaseImg3 from '../assets/projectImages/memoriesShowcaseImg3.jpg';
 
 const data = [
   {
     id: 0,
-    title: "Silon",
+    title: 'Silon',
     description:
-      "Silon is a eCommerce layout that can be used to get started with a new website for your shop. This layout is basically made of a single page of sections (hero, products, etc.). Responsive design is included.",
+      'Silon is a eCommerce layout that can be used to get started with a new website for your shop. This layout is basically made of a single page of sections (hero, products, etc.). Responsive design is included.',
     usedTechnologies:
-      "Layout was created using HTML, SCSS and builded by Webpack.",
-    date: "2021",
+      'Layout was created using HTML, SCSS and builded by Webpack.',
+    date: '2021',
     role:
-      "Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience",
-    link: "https://sunrise-afk.github.io/silon-layout/",
+      'Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience',
+    link: 'https://sunrise-afk.github.io/silon-layout/',
     jumboImg: silonJumbo,
     titleImg: silonTitle,
     showcaseImg1: silonShowcaseImg1,
@@ -54,15 +60,32 @@ const data = [
   },
   {
     id: 1,
-    title: "Reddit-Minimal",
+    title: 'Memories',
     description:
-      "Reddit-minimal is a super lightweight client of the huge reddit forum where you can read comments on posts, see the posts themselves from various subreddits and perform search by the forum. Responsive design is included",
+      'Memories is an application for posting your own memories and like memories of others. In Memories page you can signup/signin by custom json web token or simply log in with Google oAuth and create your own memories post, edit it and delete if you want. Dont be shy and like some other posts!',
     usedTechnologies:
-      "Client was created using React, Redux, React-Redux and styled by CSS modules. Asynchronous requests was implemented with Redux-Thunk.",
-    date: "2021",
+      'MERN-Stack (MongoDB, Express.js, React.js and Node.js) Application using Material-UI components for layout. There are two ways to authorize: by custom jwt token or Google oAuth.',
+    date: '2021',
     role:
-      "Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience",
-    link: "https://sunrise-afk.github.io/Reddit-minimal/",
+      'Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, BackEnd Development, Gain Experience',
+    link: 'https://sunrise-memorise-project.netlify.app',
+    jumboImg: memoriesJumbo,
+    titleImg: memoriesTitle,
+    showcaseImg1: memoriesShowcaseImg1,
+    showcaseImg2: memoriesShowcaseImg2,
+    showcaseImg3: memoriesShowcaseImg3,
+  },
+  {
+    id: 2,
+    title: 'Reddit-Minimal',
+    description:
+      'Reddit-minimal is a super lightweight client of the huge reddit forum where you can read comments on posts, see the posts themselves from various subreddits and perform search by the forum. Responsive design is included',
+    usedTechnologies:
+      'Client was created using React, Redux, React-Redux and styled by CSS modules. Asynchronous requests was implemented with Redux-Thunk.',
+    date: '2021',
+    role:
+      'Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience',
+    link: 'https://sunrise-afk.github.io/Reddit-minimal/',
     jumboImg: redditJumbo,
     titleImg: redditTitle,
     showcaseImg1: redditShowcaseImg1,
@@ -70,15 +93,15 @@ const data = [
     showcaseImg3: redditShowcaseImg3,
   },
   {
-    id: 2,
-    title: "Axit",
+    id: 3,
+    title: 'Axit',
     description:
-      "Axit is a beatiful layout which including modern design and well organised sections with easy customization possibilities. Responsive design is included.",
-    usedTechnologies: "Layout was created using React and styled with SCSS.",
-    date: "2021",
+      'Axit is a beatiful layout which including modern design and well organised sections with easy customization possibilities. Responsive design is included.',
+    usedTechnologies: 'Layout was created using React and styled with SCSS.',
+    date: '2021',
     role:
-      "Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience",
-    link: "https://sunrise-afk.github.io/axit-layout/",
+      'Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience',
+    link: 'https://sunrise-afk.github.io/axit-layout/',
     jumboImg: axitJumbo,
     titleImg: axitTitle,
     showcaseImg1: axitShowcaseImg1,
@@ -86,16 +109,16 @@ const data = [
     showcaseImg3: axitShowcaseImg3,
   },
   {
-    id: 3,
-    title: "AppLab",
+    id: 4,
+    title: 'AppLab',
     description:
       "AppLab is a layout for presenting apps. It consists a simple page made of sections (hero, pricing tables, product features and more). The overall design looks modern, pleasant and eye-catching. It's a realy great option if you need landing page for your startup!",
     usedTechnologies:
-      "Layout was created using React and styled with CSS modules.",
-    date: "2021",
+      'Layout was created using React and styled with CSS modules.',
+    date: '2021',
     role:
-      "Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience",
-    link: "https://sunrise-afk.github.io/Freebie/",
+      'Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience',
+    link: 'https://sunrise-afk.github.io/Freebie/',
     jumboImg: applabJumbo,
     titleImg: applabTitle,
     showcaseImg1: applabShowcaseImg1,
@@ -103,16 +126,16 @@ const data = [
     showcaseImg3: applabShowcaseImg3,
   },
   {
-    id: 4,
-    title: "Minimo",
+    id: 5,
+    title: 'Minimo',
     description:
-      "Minimo is a minimal but elegant layout. Minimo providing well organised sections and that makes minimo realy easy to customize for your needs. Responsive design is included.",
+      'Minimo is a minimal but elegant layout. Minimo providing well organised sections and that makes minimo realy easy to customize for your needs. Responsive design is included.',
     usedTechnologies:
-      "Layout was created using HTML, styled with SCSS and builded by Webpack.",
-    date: "2021",
+      'Layout was created using HTML, styled with SCSS and builded by Webpack.',
+    date: '2021',
     role:
-      "Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience",
-    link: "https://sunrise-afk.github.io/minimo/",
+      'Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience',
+    link: 'https://sunrise-afk.github.io/minimo/',
     jumboImg: minimoJumbo,
     titleImg: minimoTitle,
     showcaseImg1: minimoShowcaseImg1,
@@ -120,16 +143,16 @@ const data = [
     showcaseImg3: minimoShowcaseImg3,
   },
   {
-    id: 5,
-    title: "Leospa",
+    id: 6,
+    title: 'Leospa',
     description:
-      "Leospa is a landing page for Spa and beauty centers that can be used to get started with a new website for your business. This layout is basically made of a single page of sections (hero, features, testimonial, blog, contact, form, etc.). Responsive design is included.",
+      'Leospa is a landing page for Spa and beauty centers that can be used to get started with a new website for your business. This layout is basically made of a single page of sections (hero, features, testimonial, blog, contact, form, etc.). Responsive design is included.',
     usedTechnologies:
-      "Layout was created using HTML, styled with SCSS and builded by Webpack.",
-    date: "2021",
+      'Layout was created using HTML, styled with SCSS and builded by Webpack.',
+    date: '2021',
     role:
-      "Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience",
-    link: "https://sunrise-afk.github.io/leospa-layout/",
+      'Branding, Visual Identity, Web Design, Illustration, FrontEnd Development, Gain Experience',
+    link: 'https://sunrise-afk.github.io/leospa-layout/',
     jumboImg: leospaJumbo,
     titleImg: leospaTitle,
     showcaseImg1: leospaShowcaseImg1,
