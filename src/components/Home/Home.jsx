70d81a470444c72d76e4e3f4bf42531623946e11
Illia Ilin <sunrise.th13@gmail.com>
2021-02-06 13:53:03 +0200

    Fixing home component to display right second project

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { BigLink } from "../BigLink/BigLink";
-import { BorderedButton } from "../BorderedButton/BorderedButton";
-import { Footer } from "../Footer/Footer";
-import { ProjectCard } from "../HomeProjectCard/ProjectCard";
-import { ProjectCardsContainer } from "../HomeProjectCard/ProjectCardsContainer";
-import { Jumbotron } from "../Jumbotron/Jumbotron";
-import { Navbar } from "../Navbar/Navbar";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { BigLink } from '../BigLink/BigLink';
+import { BorderedButton } from '../BorderedButton/BorderedButton';
+import { Footer } from '../Footer/Footer';
+import { ProjectCard } from '../HomeProjectCard/ProjectCard';
+import { ProjectCardsContainer } from '../HomeProjectCard/ProjectCardsContainer';
+import { Jumbotron } from '../Jumbotron/Jumbotron';
+import { Navbar } from '../Navbar/Navbar';
 
 export const Home = () => {
   const projects = useSelector((state) => state.works);
@@ -23,15 +23,15 @@ export const Home = () => {
         ></ProjectCard>
         <ProjectCard
           alignRight={false}
-          projTitle={projects[2].title}
-          projImg={projects[2].jumboImg}
-          projId={projects[2].id}
+          projTitle={projects[3].title}
+          projImg={projects[3].jumboImg}
+          projId={projects[3].id}
         ></ProjectCard>
       </ProjectCardsContainer>
       <BorderedButton title="all works" link="works"></BorderedButton>
       <BigLink
-        linkPathes={["/about", "/logos"]}
-        linkTitles={["about", "logos"]}
+        linkPathes={['/about', '/logos']}
+        linkTitles={['about', 'logos']}
       ></BigLink>
       <Footer></Footer>
     </>
